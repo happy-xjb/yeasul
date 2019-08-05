c510c2d6d6c8560a86087c699389f12d8398dec3
happy-xjb <happyxjb@126.com>
2019-08-05 16:01:10 +0800

    项目可打包

--- consul-server-8500/src/main/java/com/yealink/Consul_Server_8500_APP.java
+++ consul-server-8500/src/main/java/com/yealink/Consul_Server_8500_APP.java
@@ -4,7 +4,9 @@ import com.yealink.config.Self;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 /**
@@ -15,10 +17,16 @@ import org.springframework.context.annotation.PropertySource;
 @MapperScan("com.yealink.dao")
 @EnableConfigurationProperties(Self.class)
 @PropertySource("classpath:application.yml")
-public class Consul_Server_8500_APP
+public class Consul_Server_8500_APP extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
         SpringApplication.run(Consul_Server_8500_APP.class,args);
+    }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
